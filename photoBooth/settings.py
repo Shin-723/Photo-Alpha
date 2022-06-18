3d9ffae8e76c3cdf1121ebc0b6b835389b913f65
Shin-723 <z723@naver.com>
2022-06-18 15:16:00 +0900

    deploy1

--- photoBooth/settings.py
+++ photoBooth/settings.py
@@ -90,13 +90,8 @@ WSGI_APPLICATION = 'photoBooth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',    
-        'NAME': 'photobooth',                  
-        'USER': 'shinba',                          
-        'PASSWORD': '8794',                  
-        'HOST': 'localhost',                     
-        'PORT': '3306',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
